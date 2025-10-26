64934ce036b2b8552d8c09c7488fb3cad99d7dec
nideeshkayal <nideeshkayal@gmail.com>
2025-10-27 02:33:39 +0530

    Add policies dataset

--- policies/policy.py
+++ policies/policy.py
@@ -2,8 +2,11 @@ import os
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
-def search_policies(query, policy_names=None, k=5, chroma_store_path="./chroma_store"):    
+def search_policies(query, policy_names=None, k=5):    
 
+    policies_dir = os.path.dirname(os.path.abspath(__file__))
+    chroma_store_path = os.path.join(policies_dir, "chroma_store")
+    
     embedding_model = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
@@ -12,12 +15,15 @@ def search_policies(query, policy_names=None, k=5, chroma_store_path="./chroma_s
         persist_directory=chroma_store_path,
         embedding_function=embedding_model
     )
-
+    
     if policy_names is None:
         return vectorstore.similarity_search(query, k=k)
+
+    if isinstance(policy_names, str):
+        policy_names = [policy_names]
 
     return vectorstore.similarity_search(
         query, 
         k=k, 
-        filter={"policy_name": policy_names}
+        filter={"policy_name": {"$in": policy_names}}
     )
